d1def5e82e42c0ad826aff28d6b8964f6a20e18f
Maxime <maxime.huynh@okolo.fr>
2025-05-16 22:48:24 +0200

    add multiple skeleton

--- client/src/app/[locale]/(admin)/admin/animals/AnimalsTable.tsx
+++ client/src/app/[locale]/(admin)/admin/animals/AnimalsTable.tsx
@@ -31,6 +31,7 @@ import { cn } from "@/_helpers/cn"
 import Link from "next/link"
 import { useAllAnimalsQuery } from "@/_queries/animals/useAnimalsQuery"
 import { Skeleton } from "@/_components/ui/skeleton"
+import { Group } from "@/_components/ui/group"
 
 export interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -68,137 +69,155 @@ export function AnimalsTable<TData, TValue>({ columns }: DataTableProps<TData, T
   });
 
 
-  if (isLoading) return <div className="w-full p-4">
-    <Skeleton className="rounded-xl w-full h-[500px]" />
-  </div>
-
   return (
     <div className="w-full">
-      <div className="flex items-center py-4">
-        <Link href={"/admin/animals/create"}>
-          <Button className="mr-4"><PawPrint /> Nouveau</Button>
-        </Link>
-        <Input
-          placeholder="Rechercher un nom ..."
-          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
-          onChange={(event) =>
-            table.getColumn("name")?.setFilterValue(event.target.value)
+      <div className="flex items-center justify-between py-4">
+        <Group>
+          <Link href={"/admin/animals/create"}>
+            <Button className="mr-4"><PawPrint /> Nouveau</Button>
+          </Link>
+          {isLoading ? <Skeleton className="rounded-xl w-[300px] h-[36px]" />
+            : <Input
+              placeholder="Rechercher un nom ..."
+              value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+              onChange={(event) =>
+                table.getColumn("name")?.setFilterValue(event.target.value)
+              }
+              className="max-w-sm"
+            />
           }
-          className="max-w-sm"
-        />
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="ml-auto">
-              Columns <ChevronDown />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            {table
-              .getAllColumns()
-              .filter((column) => column.getCanHide())
-              .map((column) => {
-                return (
-                  <DropdownMenuCheckboxItem
-                    key={column.id}
-                    className="capitalize"
-                    checked={column.getIsVisible()}
-                    onCheckedChange={(value) =>
-                      column.toggleVisibility(!!value)
-                    }
-                  >
-                    {column.id}
-                  </DropdownMenuCheckboxItem>
-                )
-              })}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-      <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  const pinningSide = header.column.getIsPinned();
-
+        </Group>
+        {isLoading ? <Skeleton className="rounded-xl w-[115px] h-[36px]" />
+          : <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="ml-auto">
+                Columns <ChevronDown />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {table
+                .getAllColumns()
+                .filter((column) => column.getCanHide())
+                .map((column) => {
                   return (
-                    <TableHead
-                      key={header.id}
-                      className={cn(
-                        pinningSide === 'left' && 'sticky left-0 z-10 bg-white sticky-left-shadow',
-                        pinningSide === 'right' && 'sticky right-0 z-10 bg-white sticky-right-shadow'
-                      )}
+                    <DropdownMenuCheckboxItem
+                      key={column.id}
+                      className="capitalize"
+                      checked={column.getIsVisible()}
+                      onCheckedChange={(value) =>
+                        column.toggleVisibility(!!value)
+                      }
                     >
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(header.column.columnDef.header, header.getContext())}
-                    </TableHead>
-                  );
+                      {column.id}
+                    </DropdownMenuCheckboxItem>
+                  )
                 })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => {
-                    const pinningSide = cell.column.getIsPinned();
+            </DropdownMenuContent>
+          </DropdownMenu>
+        }
+      </div>
+      {isLoading
+        ? <div className="w-full">
+          <Skeleton className="rounded-xl w-full h-[500px]" />
+        </div>
+        : <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    const pinningSide = header.column.getIsPinned();
 
                     return (
-                      <TableCell
-                        key={cell.id}
+                      <TableHead
+                        key={header.id}
                         className={cn(
                           pinningSide === 'left' && 'sticky left-0 z-10 bg-white sticky-left-shadow',
                           pinningSide === 'right' && 'sticky right-0 z-10 bg-white sticky-right-shadow'
                         )}
                       >
-                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                      </TableCell>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(header.column.columnDef.header, header.getContext())}
+                      </TableHead>
                     );
                   })}
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="flex-1 text-sm text-muted-foreground">
-          {table.getFilteredSelectedRowModel().rows.length} /{" "}
-          {table.getFilteredRowModel().rows.length} lignes sélectionnées
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => {
+                      const pinningSide = cell.column.getIsPinned();
+
+                      return (
+                        <TableCell
+                          key={cell.id}
+                          className={cn(
+                            pinningSide === 'left' && 'sticky left-0 z-10 bg-white sticky-left-shadow',
+                            pinningSide === 'right' && 'sticky right-0 z-10 bg-white sticky-right-shadow'
+                          )}
+                        >
+                          {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
         </div>
-        <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Précédent
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Suivant
-          </Button>
+      }
+      {isLoading
+        ? <Group justify="between" className="py-4">
+          <Skeleton className="rounded-xl w-[300px] h-[36px]" />
+          <Group align="center">
+            <Skeleton className="rounded-xl w-[80px] h-[36px]" />
+            <Skeleton className="rounded-xl w-[80px] h-[36px]" />
+          </Group>
+        </Group>
+        : <div className="flex items-center justify-end space-x-2 py-4">
+
+
+          <div className="flex-1 text-sm text-muted-foreground">
+            {table.getFilteredSelectedRowModel().rows.length} /{" "}
+            {table.getFilteredRowModel().rows.length} lignes sélectionnées
+          </div>
+          <div className="space-x-2">
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              Précédent
+            </Button>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              Suivant
+            </Button>
+          </div>
         </div>
-      </div>
+      }
     </div>
   )
 }
